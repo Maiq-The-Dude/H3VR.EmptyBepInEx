e3189b4fcada457c27f108def59b09ab057018e1
M'aiq <maiqtellssome@gmail.com>
2021-06-15 01:24:23 -0500

    Add files

---
+++ Plugin/Plugin/src/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+
+namespace InfiniteCasings
+{
+	[BepInPlugin("maiq.Plugin", "Plugin", "0.1.0")]
+	[BepInProcess("h3vr.exe")]
+	public class InfiniteCasings : BaseUnityPlugin
+	{
+		public InfiniteCasings()
+		{
+			Hook();
+		}
+
+		private void OnDestroy()
+		{
+			Unhook();
+		}
+
+		private void Hook()
+		{
+
+		}
+
+		private void Unhook()
+		{
+
+		}
+	}
+}
